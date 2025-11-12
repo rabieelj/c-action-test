5ef7a363af0e54551290eee3dfc09317f4584943
Rabie <jabpro1@gmail.com>
2025-11-12 15:34:21 +0100

    create a function

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+int add(int a , int b ) ;
 
 int main(){
-    printf("hello from c")
+    printf("hello from c");
     return 0;
+}
+int  add(int a , int b ) {
+    return a + b ;
 }
